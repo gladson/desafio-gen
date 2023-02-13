166baa0abaa21eada2eb01417bcb6e15e70852f2
gladson <gladsonbrito@gmail.com>
2023-02-12 23:01:01 -0400

    add(constants): app name and route initial and base url image background and base url mock

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+const String envNameApp = "Marvel";
+const String envRouteInitial = "/home";
+const String envBaseURLImageBackground =
+    "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
+const String envBaseURLMock = ("https://gist.githubusercontent.com/gladson/"
+    "37cac235c1760f25807b110b2b9368c6/raw/mock.json");
